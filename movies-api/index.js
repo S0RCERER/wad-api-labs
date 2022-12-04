d3df30fbba769131d50d3a550b30d3db4c510341
Ling Feng <20099866@mail.wit.ie>
2022-12-04 19:21:12 +0000

    added routing for movies and movie reviews

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
+app.use('/api/genres', genresRouter);
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
