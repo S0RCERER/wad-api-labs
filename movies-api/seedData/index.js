dc14fedc125830703098b08e33aa063097f3de0b
Ling Feng <20099866@mail.wit.ie>
2022-12-05 01:33:30 +0000

    Mongo lab Exercises done

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,21 +1,21 @@
-import userModel from '../api/users/userModel';
-import users from './users';
+import genreModel from '../api/genres/genreModel';
+import genres from './genres';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// deletes all user documents in collection and inserts test data
-async function loadUsers() {
-  console.log('load user Data');
+// deletes all genre documents in collection and inserts test data
+async function loadGenres() {
+  console.log('load genre Data');
   try {
-    await userModel.deleteMany();
-    await userModel.collection.insertMany(users);
-    console.info(`${users.length} users were successfully stored.`);
+    await genreModel.deleteMany();
+    await genreModel.collection.insertMany(genres);
+    console.info(`${genres.length} genres were successfully stored.`);
   } catch (err) {
-    console.error(`failed to Load user Data: ${err}`);
+    console.error(`failed to Load genre Data: ${err}`);
   }
 }
 
 if (process.env.SEED_DB) {
-  loadUsers();
+  loadGenres();
 }
