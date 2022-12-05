2e055f76e74ec1a6e9fcc15f39311082a71103c8
Ling Feng <20099866@mail.wit.ie>
2022-12-05 12:01:22 +0000

    Topic 11 - Salt and Hash

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,21 +1,21 @@
-import genreModel from '../api/genres/genreModel';
-import genres from './genres';
+import userModel from '../api/users/userModel';
+import users from './users';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// deletes all genre documents in collection and inserts test data
-async function loadGenres() {
-  console.log('load genre Data');
+// deletes all user documents in collection and inserts test data
+async function loadUsers() {
+  console.log('load user Data');
   try {
-    await genreModel.deleteMany();
-    await genreModel.collection.insertMany(genres);
-    console.info(`${genres.length} genres were successfully stored.`);
+    await userModel.deleteMany();
+    await users.forEach(user => userModel.create(user));
+    console.info(`${users.length} users were successfully stored.`);
   } catch (err) {
-    console.error(`failed to Load genre Data: ${err}`);
+    console.error(`failed to Load user Data: ${err}`);
   }
 }
 
 if (process.env.SEED_DB) {
-  loadGenres();
+  loadUsers();
 }
